c5a705a2f82fc087cd089441c214fa84a4de6411
EmhaHasyim <redeath001@gmail.com>
2024-04-26 00:20:17 +0700

    fix btn cannot search

--- src/utils/SearchBar/SearchBar.tsx
+++ src/utils/SearchBar/SearchBar.tsx
@@ -21,9 +21,15 @@ const SearchBar = () => {
         coverBlack?.classList.toggle('hidden')
     }
 
-    const handleBtnSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {  // Gunakan tipe event yang benar
-        if (event.key === 'Enter' && searchRef.current) {  // Periksa jika ref bukan null
-            navigate(`/search/${searchRef.current.value}`, {relative: 'path'})  // Gunakan nilai dari input
+    const handleInputSearch = (event: React.KeyboardEvent<HTMLInputElement>) => { 
+        if (event.key === 'Enter' && searchRef.current) {
+            navigate(`/search/${searchRef.current.value}`, {relative: 'path'})
+        }
+    }
+
+    const handleBtnSearch = () => {
+        if(searchRef.current){
+            navigate(`/search/${searchRef.current.value}`, {relative: 'path'})
         }
     }
     
@@ -34,8 +40,8 @@ const SearchBar = () => {
             </button>
             <section id="searchBarInput" className="z-[1] hidden absolute duration-75 w-full h-12 left-0 top-12 flex justify-center items-center bg-theme-light dark:bg-theme-dark border-t-2 border-theme-light ease-in-out transition-all">
                 <label htmlFor="searchValue" className="w-8/12 relative">
-                    <input onKeyDown={handleBtnSearch} ref={searchRef} type="text" placeholder="Search..." className="w-full px-2 py-0.5 h-8 focus:outline-none bg-transparent border-2 rounded  dark:border-dark-background-primary border-light-background-primary" form="searchValue" />
-                    <button onClick={() => console.log('search')} className="absolute right-0 w-8 h-8 border-y-2 border-r-2 rounded-r bg-dark-background-primary dark:border-dark-background-primary">
+                    <input onKeyDown={handleInputSearch} ref={searchRef} type="text" placeholder="Search..." className="w-full px-2 py-0.5 h-8 focus:outline-none bg-transparent border-2 rounded  dark:border-dark-background-primary border-light-background-primary" form="searchValue" />
+                    <button onClick={() => handleBtnSearch} className="absolute right-0 w-8 h-8 border-y-2 border-r-2 rounded-r bg-dark-background-primary dark:border-dark-background-primary">
                         <MagnifyingGlass size={23} weight="bold" className="mx-auto" />
                     </button>
                 </label>
