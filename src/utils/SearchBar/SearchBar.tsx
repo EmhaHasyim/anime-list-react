590d7d90da6729809a98278fbf9e2c2b332a89b2
EmhaHasyim <redeath001@gmail.com>
2024-04-25 21:40:32 +0700

    fix size input search

--- src/utils/SearchBar/SearchBar.tsx
+++ src/utils/SearchBar/SearchBar.tsx
@@ -15,8 +15,8 @@ const SearchBar = () => {
             <button onClick={() => handleBtnToDisplaySearchBarInput()}>
                 <MagnifyingGlass size={32} weight="bold" />
             </button>
-            <section id="searchBarInput" className="z-[1] hidden absolute w-full h-12 left-0 top-12 flex justify-center items-center bg-theme-light dark:bg-theme-dark border-t-2 border-theme-light ease-in-out transition-all">
-                <label htmlFor="searchValue" className="w-1/2 relative">
+            <section id="searchBarInput" className="z-[1] hidden absolute duration-75 w-full h-12 left-0 top-12 flex justify-center items-center bg-theme-light dark:bg-theme-dark border-t-2 border-theme-light ease-in-out transition-all">
+                <label htmlFor="searchValue" className="w-8/12 relative">
                     <input type="text" placeholder="Search..." className="w-full px-2 py-0/.5 h-8 focus:outline-none bg-transparent border-2 rounded  dark:border-dark-background-primary border-light-background-primary" form="searchValue" />
                     <button onClick={() => console.log('search')} className="absolute right-0 w-8 h-8 border-y-2 border-r-2 rounded-r bg-dark-background-primary dark:border-dark-background-primary">
                         <MagnifyingGlass size={23} weight="bold" className="mx-auto" />
