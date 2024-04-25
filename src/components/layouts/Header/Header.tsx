e4381ac942440ed32b70711f59ba71fc026a709a
EmhaHasyim <redeath001@gmail.com>
2024-04-25 21:15:22 +0700

    fix hidden

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -4,9 +4,9 @@ import SearchBar from "@/utils/SearchBar/SearchBar"
 const Header = () => {
     return (
         <>
-            <header className="h-12 px-5 flex items-center justify-between dark:bg-theme-dark bg-theme-light">
+            <header className="z-20 h-12 px-5 flex items-center justify-between dark:bg-theme-dark bg-theme-light">
                 <a className="font-bold text-3xl sm:text-red-500" href="/">AnimeList</a>
-                <section className="flex items-center justify-center gap-3">
+                <section className="flex items-center justify-center gap-3 overflow-hidden">
                     <DarkModeBtn />
                     <SearchBar/>
                 </section>
