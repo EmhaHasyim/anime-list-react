59f33893221219eeffa729811d762da3ccda72b7
EmhaHasyim <redeath001@gmail.com>
2024-05-02 18:11:28 +0700

    add a interface for detail anime

--- src/utils/Fetcher/Fetcher.tsx
+++ src/utils/Fetcher/Fetcher.tsx
@@ -1,8 +1,9 @@
+import DetailAnime from "../Interface/DetailAnime"
 import SearchAnime from "../Interface/SearchAnime"
 import SeasonsNow from "../Interface/SeasonsNow"
 import TopAnime from "../Interface/TopAnime"
 
-const Fetcher = async (url: string): Promise<SearchAnime | TopAnime | SeasonsNow | null | any> => {
+const Fetcher = async (url: string): Promise<SearchAnime | TopAnime | SeasonsNow | DetailAnime | null | any> => {
     try{
         const res = await fetch(`${import.meta.env.VITE_API_URL}${url}`)
         const data = await res.json()
