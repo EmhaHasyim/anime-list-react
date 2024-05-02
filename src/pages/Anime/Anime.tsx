fe74d7339375aaba93f51806588589cf83f23d91
EmhaHasyim <redeath001@gmail.com>
2024-05-02 18:11:43 +0700

    add a trailer

--- src/pages/Anime/Anime.tsx
+++ src/pages/Anime/Anime.tsx
@@ -1,5 +1,48 @@
+import Fetcher from "@/utils/Fetcher/Fetcher"
+import DetailAnime from "@/utils/Interface/DetailAnime"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import Trailer from "./TrailerVideo/Trailer"
+
+
 const Anime = () => {
-    return <>Detail Anime</>
+    // params
+    const params = useParams()
+
+    // nilai id anime
+    const idAnime = params.id
+
+    // data untuk detail anime
+    const [dataDetailAnime, setDataDetailAnime] = useState<DetailAnime | null>(null)
+
+    // kondisi untuk error
+    const [error, setError] = useState<boolean>(false)
+
+    // kondisi untuk loading
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+
+    useEffect(() => {
+        const fetchDataDetailAnime = async () => {
+            try {
+                const res = await Fetcher(`/anime/${idAnime}/full`)
+                setDataDetailAnime(res)
+            } catch (err) {
+                setError(true)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        fetchDataDetailAnime()
+    }, [idAnime])
+
+    if (error) return <>Reload Page</>
+    if (isLoading) return <>Loading....</>
+
+    return (
+        <>
+        <Trailer LinkVideo={dataDetailAnime?.data.trailer.embed_url} title={dataDetailAnime?.data.title}/>
+        </>
+    )
 }
 
 export default Anime
