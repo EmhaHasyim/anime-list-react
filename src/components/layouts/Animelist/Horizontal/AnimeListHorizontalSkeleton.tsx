5cc16f6cf8abf6c677ef370d650aebf302939e43
EmhaHasyim <redeath001@gmail.com>
2024-05-14 12:58:35 +0700

    fix a horizontal animelist

--- src/components/layouts/Animelist/Horizontal/AnimeListHorizontalSkeleton.tsx
+++ src/components/layouts/Animelist/Horizontal/AnimeListHorizontalSkeleton.tsx
@@ -6,7 +6,7 @@ const AnimeListHorizontalSkeleton = () => {
         </section>
         <section className="overflow-x-auto h-56 mt-1">
             <ul className="whitespace-nowrap flex gap-2">
-                {Array(25).fill(0).map((_, index) => (
+                {Array(16).fill(0).map((_, index) => (
                     <li key={index}>
                         <div className='w-full h-56 aspect-[2/3] rounded dark:bg-skeleton-dark bg-skeleton-light animate-pulse'></div>
                     </li>
