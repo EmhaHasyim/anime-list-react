ca046c6e73f23354190f2bd28d0fac4c8d33f269
EmhaHasyim <redeath001@gmail.com>
2024-05-14 13:49:34 +0700

    change animelist horizontal to responsove

--- src/components/layouts/Animelist/Horizontal/AnimeListHorizontal.tsx
+++ src/components/layouts/Animelist/Horizontal/AnimeListHorizontal.tsx
@@ -2,7 +2,7 @@ import TopAnime from "@/utils/Interface/TopAnime"
 import { Link } from "react-router-dom"
 import { TagSimple, Star, User, DotsThreeOutline } from "@phosphor-icons/react"
 
-const AnimeListHorizontal = ({ anime, rank , path}: { anime: TopAnime | null, rank: boolean , path: string}) => {
+const AnimeListHorizontal = ({ anime, rank, path }: { anime: TopAnime | null, rank: boolean, path: string }) => {
 
 
     // kondisi jika rank true
@@ -30,24 +30,24 @@ const AnimeListHorizontal = ({ anime, rank , path}: { anime: TopAnime | null, ra
                         }
                         return (
                             <li key={anime.mal_id}>
-                                <section className="relative h-56 w-full aspect-[2/3] bg-cover bg-center bg-no-repeat overflow-hidden rounded">
-                                    <img src={anime.images.webp.large_image_url} alt='poster' loading="lazy"/>
+                                <section className="relative h-56 w-full aspect-[2/3] overflow-hidden rounded sm:h-60 md:h-64 lg:h-72">
+                                    <img className="h-full w-full" src={anime.images.webp.image_url} alt='poster' loading="lazy" />
                                     <Link to={`/anime/${anime.mal_id}`} className="absolute bg-transparent dark:bg-transparent w-full h-full left-0 top-0" />
-                                    <section className="absolute bg-black bg-opacity-70 bottom-0 left-0 h-10 w-full flex flex-col py-0.5 px-1 text-xs text-white justify-center items-left">
+                                    <section className="absolute bg-black bg-opacity-70 bottom-0 left-0 h-10 w-full flex flex-col py-0.5 px-1 text-xs text-white justify-center items-left md:text-sm md:py-1">
                                         {tagRank(rank, anime.rank)}
-                                        <section className="title">
+                                        <section className="title flex items-center justify-start">
                                             <h1 className="font-medium truncate">
                                                 {anime.title}
                                             </h1>
                                         </section>
-                                        <section className="flex items-center gap-[3px]">
-                                            <p className="flex items-center text-[10px]">
+                                        <section className="flex justify-start items-center gap-[3px] sm:gap-1 md:gap-[5px] lg:gap-[6px] text-[10px] sm:text-[11px] md:text-[12px] lg:text-sm">
+                                            <p className="flex items-center">
                                                 {anime.type}({anime.episodes})
                                             </p>
-                                            <span className="flex gap-[1px] items-center text-[10px]">
+                                            <span className="flex gap-[1px] items-center justify-center">
                                                 <Star size={13} /> {anime.score}
                                             </span>
-                                            <span className="flex gap-[1px] items-center text-[10px]">
+                                            <span className="flex gap-[1px] items-center justify-center">
                                                 <User size={13} weight="fill" /> {membersFix(anime.members)}
                                             </span>
                                         </section>
@@ -57,7 +57,7 @@ const AnimeListHorizontal = ({ anime, rank , path}: { anime: TopAnime | null, ra
                         )
                     }) : null}
                     <li>
-                        <section className="relative h-56 w-full aspect-[2/3] flex items-center justify-center rounded dark:bg-dark-background-secondary bg-light-background-secondary">
+                        <section className="relative h-56 w-full aspect-[2/3] flex items-center justify-center rounded dark:bg-dark-background-secondary bg-light-background-secondary sm:h-60 md:h-64 lg:h-72">
                             <Link to={path} className=" text-xl flex items-center justify-center flex-col text-center text-theme-light dark:text-theme-dark hover:text-theme-dark dark:hover:text-theme-light underline">
                                 <DotsThreeOutline size={45} />
                                 See More</Link>
--- src/components/layouts/Animelist/Horizontal/AnimeListHorizontalHeader.tsx
+++ src/components/layouts/Animelist/Horizontal/AnimeListHorizontalHeader.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const AnimeListHorizontalHeader = ({title, path}: {title: string, path: string}) => {
     return(
         <>
-            <section className="w-full flex items-center justify-between px-2 py-0.5">
+            <section className="w-full flex items-center justify-between h-6 px-2 py-1 mb-1 text-base sm:text-lg sm:h-7 md:text-xl md:h-8 lg:text-2xl lg:h-9">
                 <h1 className="font-medium">{title}</h1>
                 <Link to={path} className="font-medium text-theme-light hover:text-theme-dark dark:text-theme-dark dark:hover:text-theme-light underline">See More</Link>
             </section>
