89d004b1dc65bb42f59c824b3564ee4727274c59
EmhaHasyim <redeath001@gmail.com>
2024-06-09 01:16:49 +0700

    optimize, cleand and use axios

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,64 +1,41 @@
 import { useEffect, useState } from "react"
 import { useParams, useSearchParams } from "react-router-dom"
-import Fetcher from "@/utils/Fetcher/Fetcher"
 import AnimeListVertikal from "@/components/layouts/Animelist/Vertikal/AnimeListVertikal"
 import SearchAnime from "@/utils/Interface/SearchAnime"
 import AnimeListVertikalSkeleton from "@/components/layouts/Animelist/Vertikal/AnimeListVertikalSkeleton"
+import axios from "axios"
+
+const apiUrl = import.meta.env.VITE_API_URL
 
 const Search = () => {
-    // params
     const params = useParams()
-
-    // search params
     const [searchParams] = useSearchParams()
 
-    // mengambil params dari valueSearch
     const valueSearch: string | undefined = params.valueSearch
-
-    // mengambil params dari type
     const valueType = searchParams.get('type')
-
-    // data untuk search anime
     const [dataSearchAnime, setDataSearchAnime] = useState<null | SearchAnime>(null)
 
-    // kondisi untuk error
     const [error, setError] = useState<boolean>(false)
-
-    // kondisi untuk loading
     const [isLoading, setIsLoading] = useState<boolean>(true)
-
-    // mengambil data saat halaman dimasuki
     useEffect(() => {
         const fetchDataSearch = async () => {
             try {
-                // mengambil data dari api
-                const res = await Fetcher(`/anime?q=${valueSearch}&type=${valueType}`)
-
-                // mengurutkan dari member terbesar
+                const res = await (await axios.get(`${apiUrl}/anime?q=${valueSearch}&type=${valueType}`)).data
                 res.data.sort((a: { members: number }, b: { members: number }) => b.members - a.members)
-
-                // mengset data dari api
                 setDataSearchAnime(res)
             } catch (err) {
-                // merubah kondisi error
                 setError(true)
             } finally {
-                // merubah kondisi loading
                 setIsLoading(false)
             }
         }
         fetchDataSearch()
     }, [valueSearch, valueType])
 
-    // menghilangkan %
     const valueSearchFix = (): string | undefined => {
         return valueSearch?.replace(/%/g, "")
     }
-
-    // kondisi error
     if (error) { return <>Reaload Page</> }
-
-    // kondisi loading
     if (isLoading) {
         return (
             <>
@@ -67,7 +44,6 @@ const Search = () => {
             </>
         )
     }
-
 
     return (
         <>
